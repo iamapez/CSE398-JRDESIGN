a1cd4d7029e1b26fa5520b97065f1d3db0ef89c7
Alex Perez <acperez@syr.edu>
2022-04-18 14:38:25 -0400

    working test qrcode shell code

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 import cv2
 import webbrowser
+import pyqrcode
+from PIL import Image
+from pyzbar import pyzbar
+import png
 
 
-def testQRDetection():
-    cap = cv2.VideoCapture(4)
-    # initialize the cv2 QRCode detector
-    detector = cv2.QRCodeDetector()
-
-    while True:
-        _, img = cap.read()
-        # detect and decode
-        data, bbox, _ = detector.detectAndDecode(img)
-        # check if there is a QRCode in the image
-        if data:
-            a = data
-            break
-        cv2.imshow("QRCODEscanner", img)
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-    b = webbrowser.open(str(a))
-    cap.release()
-    cv2.destroyAllWindows()
+def generateQRCode():
+    qr = pyqrcode.create('alexTEST')
+    qr.png('alexTEST.png', scale=6)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('testing here')
-#     test
+def decodeQR():
+    img_path = 'alexTEST.png'
+    img = cv2.imread(img_path)
+
+    data = pyzbar.decode(img)
+
+    print(data)
+
+
+def testQR():
+    generateQRCode()
+    decodeQR()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    testQRDetection()
-
+    testQR()
+    exit(1)
