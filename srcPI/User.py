24328f81261443fc88ffcb7e05b0e77e1e7cbd1a
Alex Perez <acperez@syr.edu>
2022-04-20 14:20:13 -0400

    randomly generate info and write to json file

--- srcPI/User.py
+++ srcPI/User.py
@@ -1,20 +1,13 @@
 import json
 
-class User:
-#     def __init__(self):
-#         self.uuid = None
-#         self.balance = None
-#         self.name = None
-#         self.carColor = None
-#         self.plateNumber = None
 
-    def __init__(self,uuid=None,balance=None,name=None,carColor=None,plateNumber=None,twitterusername=None, pathToQRCODE=None):
+class User:
+    def __init__(self, uuid=None, balance=None, name=None, carColor=None, plateNumber=None, pathToQRCODE=None):
         self.uuid = uuid
         self.balance = balance
         self.name = name
         self.carColor = carColor
         self.plateNumber = plateNumber
-        self.twitterusername = twitterusername
         self.pathToQRCODE = pathToQRCODE
 
     def getUUID(self):
@@ -32,13 +25,13 @@ class User:
     def getName(self):
         return self.name
 
-    def setName(self,name):
+    def setName(self, name):
         self.name = name
 
     def getCarColor(self):
         return self.carColor
 
-    def setCarColor(self,color):
+    def setCarColor(self, color):
         self.carColor = color
 
     def getPlateNumber(self):
@@ -46,12 +39,6 @@ class User:
 
     def setPlateNumber(self, val):
         self.plateNumber = val
-
-    def gettwitterusername(self):
-        return self.twitterusername
-
-    def settwitterusername(self, username):
-        self.twitterusername = username
 
     def getPathToQRCode(self):
         return self.pathToQRCODE
